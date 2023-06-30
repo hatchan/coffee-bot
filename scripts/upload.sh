1215da73b28d535d8b18db7bc3b97d57cf712aa5
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-06-30 13:56:08 +0200

    update upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+cargo zigbuild --target arm-unknown-linux-gnueabihf --release
+
 scp \
     ./target/arm-unknown-linux-gnueabihf/release/coffee-bot \
     coffee-bot@${COFFEE_BOT_IP}:/home/coffee-bot/coffee-bot
