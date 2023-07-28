12040cf580a17671baf99567b0df203a9bd6eac0
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-07-28 19:11:51 +0200

    new script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -6,4 +6,4 @@ cargo zigbuild --target arm-unknown-linux-gnueabihf --release
 
 scp \
     ./target/arm-unknown-linux-gnueabihf/release/coffee-bot \
-    coffee-bot@${COFFEE_BOT_IP}:/home/coffee-bot/coffee-bot
+    coffee-bot@${COFFEE_BOT_IP}:/home/coffee-bot/coffee-bot-new
